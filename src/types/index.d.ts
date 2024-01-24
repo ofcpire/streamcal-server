5aaf0da296b2c936ea298c4861e8fd8bb75ce43e
ofcpire <ofcpire@gmail.com>
2024-01-25 02:03:41 +0900

    Feat: 전역 타입 설정

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+interface ChannelInfoType {
+  channelId: string;
+  channelName: string;
+  channelImageUrl: string;
+  verifiedMark: boolean;
+  channelType: string;
+  channelDescription: string;
+  followerCount: number;
+  openLive: boolean;
+}
