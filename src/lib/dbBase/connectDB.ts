ba9f1706a3af41d12dcd43178b82d57b3b3cdeb8
ofcpire <ofcpire@gmail.com>
2024-01-31 20:04:47 +0900

    Feat: 원격 db 대응 및 env 추가

--- src/lib/dbBase/connectDB.ts
+++ src/lib/dbBase/connectDB.ts
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
+const isDBSecured = process.env.IS_DB_SECURED;
+const dbId = process.env.DB_ID;
+const dbPw = process.env.DB_PW;
+const dbAddress = process.env.DB_ADDRESS;
+const dbPort = process.env.DB_PORT;
 
 // MONGOOSE CONNECT
 const connectDB = (databaseName: string) => {
+  const dbConnectString = isDBSecured
+    ? `mongodb://${dbId}:${dbPw}@${dbAddress}:${dbPort}/${databaseName}`
+    : `mongodb://${dbAddress}:${dbPort}/${databaseName}`;
+  console.log(dbConnectString);
   mongoose
     .connect(`mongodb://127.0.0.1:27017/${databaseName}`)
     .then(() => {
